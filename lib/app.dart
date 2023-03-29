169803fc64606a995c979f626ee984985466dd32
Alemartech <alemar83@gmail.com>
2023-03-29 19:31:54 +0200

    inizializzazione progetto: localizzazione

--- lib/app.dart
+++ lib/app.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class OfferteLavoroFlutterApp extends StatelessWidget {
   const OfferteLavoroFlutterApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      supportedLocales: AppLocalizations.supportedLocales,
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+    );
   }
 }
